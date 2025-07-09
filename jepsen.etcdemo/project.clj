5691d8535585456bb7f0ce2a1abb7546b09b0222
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-07-09 17:36:37 +0300

    Add demo

---
+++ jepsen.etcdemo/project.clj
@@ -0,0 +1,9 @@
+(defproject jepsen.etcdemo "0.1.0-SNAPSHOT"
+  :description "A Jepsen test for etcd"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [jepsen "0.2.1-SNAPSHOT"]
+                 [verschlimmbesserung "0.1.3"]]
+  :repl-options {:init-ns jepsen.etcdemo}
+  :main jepsen.etcdemo)
