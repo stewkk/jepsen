50ad5f6a7c56292f51e398794cf10069b789d015
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-07-13 15:48:11 +0300

    Refactor tests

--- jepsen.etcdemo/src/jepsen/etcdemo.clj
+++ jepsen.etcdemo/src/jepsen/etcdemo.clj
@@ -16,37 +16,14 @@
             [verschlimmbesserung.core :as v]
             [slingshot.slingshot :refer [try+]]
             [knossos.model :as model]
-            [jepsen.independent :as independent]
-            [jepsen.checker :as checker]))
+            [jepsen.etcdemo [support :as s]
+             [set :as set]]
+            [jepsen.etcdemo.set :as set]))
 
 (def dir     "/opt/etcd")
 (def binary "etcd")
 (def logfile (str dir "/etcd.log"))
 (def pidfile (str dir "/etcd.pid"))
-
-(defn node-url
-  "An HHTP url for connecting to a node on a particular port."
-  [node port]
-  (str "http://" node ":" port))
-
-(defn peer-url
-  "The HTTP url for other peers to talk to a node."
-  [node]
-  (node-url node 2380))
-
-(defn client-url
-  "The HTTP url clients use to talk to a node."
-  [node]
-  (node-url node 2379))
-
-(defn initial-cluster
-  "Constructs an initial cluster string for a test, like
-  \"foo=foo:2380,bar=bar:2380,...\""
-  [test]
-  (->> (:nodes test)
-       (map (fn [node]
-              (str node "=" (peer-url node))))
-       (str/join ",")))
 
 (defn db
   "Etcd DB for a particular version."
@@ -65,12 +42,12 @@
         binary
         :--log-output                   :stderr
         :--name                         (name node)
-        :--listen-peer-urls             (peer-url   node)
-        :--listen-client-urls           (client-url node)
-        :--advertise-client-urls        (client-url node)
+        :--listen-peer-urls             (s/peer-url   node)
+        :--listen-client-urls           (s/client-url node)
+        :--advertise-client-urls        (s/client-url node)
         :--initial-cluster-state        :new
-        :--initial-advertise-peer-urls  (peer-url node)
-        :--initial-cluster              (initial-cluster test))
+        :--initial-advertise-peer-urls  (s/peer-url node)
+        :--initial-cluster              (s/initial-cluster test))
        (Thread/sleep 1000)))
 
     (teardown! [_ _ node]
@@ -95,7 +72,7 @@
 (defrecord Client [conn]
   client/Client
   (open! [this _ node]
-    (assoc this :conn (v/connect (client-url node)
+    (assoc this :conn (v/connect (s/client-url node)
                                  {:timeout 5000})))
 
   (setup! [_ _])
@@ -129,53 +106,78 @@
 
   (close! [_ _]))
 
+(defn register-workload
+  "Tests linearizable reads, writes, and compare-and-set operations on
+  independent keys."
+  [opts]
+  {:client    (Client. nil)
+   :checker   (independent/checker
+               (checker/compose
+                {:linear   (checker/linearizable {:model     (model/cas-register)
+                                                  :algorithm :linear})
+                 :timeline (timeline/html)}))
+   :generator (independent/concurrent-generator
+               10
+               (range)
+               (fn [_]
+                 (->> (gen/mix [r w cas])
+                      (gen/limit (:ops-per-key opts)))))})
+
+(def workloads
+  "A map of workload names to functions that construct workloads, given opts."
+  {"set"      set/workload
+   "register" register-workload})
+
 (defn etcd-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
   :concurrency, ...), constructs a test map."
   [opts]
-  (let [quorum (boolean (:quorum opts))]
-
+  (let [quorum (boolean (:quorum opts))
+        workload ((get workloads (:workload opts)) opts)]
     (merge tests/noop-test
-           {:name (str "etcd q=" quorum)
+           opts
+           {:name (str "etcd q=" quorum " " (name (:workload opts)))
             :os debian/os
             :db (db "v3.1.5")
             :pure-generators true
             :quorum quorum
-            :client (Client. nil)
+            :client (:client workload)
             :checker (checker/compose
                       {:perf   (checker/perf)
                        :indep (independent/checker
                                (checker/compose
-                                {:linear (checker/linearizable {:model     (model/cas-register)
-                                                                :algorithm :linear})
+                                {:workload (:checker workload)
                                  :timeline (timeline/html)}))})
             :nemesis (nemesis/partition-random-halves)
-            :generator (->> (independent/concurrent-generator
-                             10
-                             (range)
-                             (fn [_]
-                               (->> (gen/mix [r w cas])
-                                    (gen/stagger (/ (:rate opts)))
-                                    (gen/limit (:ops-per-key opts)))))
-                            (gen/nemesis
-                             (cycle [(gen/sleep 5)
-                                     {:type :info, :f :start}
-                                     (gen/sleep 5)
-                                     {:type :info, :f :stop}]))
-                            (gen/time-limit (:time-limit opts)))}
-           opts)))
+            :generator (gen/phases
+                        (->> (:generator workload)
+                             (gen/stagger (/ (:rate opts)))
+                             (gen/nemesis
+                              (cycle [(gen/sleep 5)
+                                      {:type :info, :f :start}
+                                      (gen/sleep 5)
+                                      {:type :info, :f :stop}]))
+                             (gen/time-limit (:time-limit opts)))
+                        (gen/log "Healing cluster")
+                        (gen/nemesis (gen/once {:type :info, :f :stop}))
+                        (gen/log "Waiting for recovery")
+                        (gen/sleep 10)
+                        (gen/clients (:final-generator workload)))})))
 
 (def cli-opts
   "Additional command line options."
-    [["-q" "--quorum" "Use quorum reads, instead of reading from any primary."]
-     ["-r" "--rate HZ" "Approximate number of requests per second, per thread."
-      :default  10
-      :parse-fn read-string
-      :validate [#(and (number? %) (pos? %)) "Must be a positive number"]]
-     [nil "--ops-per-key NUM" "Maximum number of operations on any given key."
-      :default  100
-      :parse-fn parse-long
-      :validate [pos? "Must be a positive integer."]]])
+  [["-q" "--quorum" "Use quorum reads, instead of reading from any primary."]
+   ["-r" "--rate HZ" "Approximate number of requests per second, per thread."
+    :default  10
+    :parse-fn read-string
+    :validate [#(and (number? %) (pos? %)) "Must be a positive number"]]
+   [nil "--ops-per-key NUM" "Maximum number of operations on any given key."
+    :default  100
+    :parse-fn parse-long
+    :validate [pos? "Must be a positive integer."]]
+   ["-w" "--workload NAME" "What workload should we run?"
+    :missing  (str "--workload " (cli/one-of workloads))
+    :validate [workloads (cli/one-of workloads)]]])
 
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
