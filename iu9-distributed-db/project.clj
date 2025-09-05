bcb9551e2738ac12cfbb96e63534799c72a01b3d
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-09-05 16:26:15 +0300

    Remove etcd related code

--- iu9-distributed-db/project.clj
+++ iu9-distributed-db/project.clj
@@ -3,7 +3,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [jepsen "0.3.10-SNAPSHOT"]
-                 [verschlimmbesserung "0.1.3"]]
+                 [jepsen "0.3.10-SNAPSHOT"]]
   :repl-options {:init-ns jepsen.iu9db}
   :main jepsen.iu9db)
