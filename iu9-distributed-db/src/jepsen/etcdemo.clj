d4cc27b53d7c39f493006be8c559970a776ac108
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-09-05 15:43:00 +0300

    Add iu9-distibuted-db test

---
+++ iu9-distributed-db/src/jepsen/etcdemo.clj
@@ -0,0 +1,189 @@
+(ns jepsen.etcdemo
+  (:require [clojure.tools.logging :refer [info]]
+            [clojure.string :as str]
+            [jepsen [cli :as cli]
+             [control :as c]
+             [db :as db]
+             [tests :as tests]
+             [generator :as gen]
+             [client :as client]
+             [checker :as checker]
+             [nemesis :as nemesis]
+             [independent :as independent]]
+            [jepsen.checker.timeline :as timeline]
+            [jepsen.control.util :as cu]
+            [jepsen.os.debian :as debian]
+            [verschlimmbesserung.core :as v]
+            [slingshot.slingshot :refer [try+]]
+            [knossos.model :as model]
+            [jepsen.etcdemo [support :as s]
+             [set :as set]]
+            [jepsen.etcdemo.set :as set]))
+
+(def dir     "/opt/etcd")
+(def binary "etcd")
+(def logfile (str dir "/etcd.log"))
+(def pidfile (str dir "/etcd.pid"))
+
+(defn db
+  "Etcd DB for a particular version."
+  [version]
+  (reify db/DB
+    (setup! [_ test node]
+      (info node "installing etcd" version)
+      (c/su
+       (let [url (str "https://storage.googleapis.com/etcd/" version
+                      "/etcd-" version "-linux-amd64.tar.gz")]
+         (cu/install-archive! url dir))
+       (cu/start-daemon!
+        {:logfile logfile
+         :pidfile pidfile
+         :chdir dir}
+        binary
+        :--log-output                   :stderr
+        :--name                         (name node)
+        :--listen-peer-urls             (s/peer-url   node)
+        :--listen-client-urls           (s/client-url node)
+        :--advertise-client-urls        (s/client-url node)
+        :--initial-cluster-state        :new
+        :--initial-advertise-peer-urls  (s/peer-url node)
+        :--initial-cluster              (s/initial-cluster test))
+       (Thread/sleep 1000)))
+
+    (teardown! [_ _ node]
+      (info node "tearing down etcd")
+      (cu/stop-daemon! binary pidfile)
+      (c/su (c/exec :rm :-rf dir)))
+
+    db/LogFiles
+    (log-files [_ _ _]
+      [logfile])))
+
+(defn r   [_ _] {:type :invoke, :f :read, :value nil})
+(defn w   [_ _] {:type :invoke, :f :write, :value (rand-int 5)})
+(defn cas [_ _] {:type :invoke, :f :cas, :value [(rand-int 5) (rand-int 5)]})
+
+(defn parse-long-nil
+  "Parses a string to a Long. Passes through `nil`."
+  [s]
+  (when s (parse-long s)))
+
+
+(defrecord Client [conn]
+  client/Client
+  (open! [this _ node]
+    (assoc this :conn (v/connect (s/client-url node)
+                                 {:timeout 5000})))
+
+  (setup! [_ _])
+
+  (invoke! [_ test op]
+    (let [[k v] (:value op)]
+      (try+
+       (case (:f op)
+         :read (let [value (-> conn
+                               (v/get k {:quorum? (:quorum test)})
+                               parse-long-nil)]
+                 (assoc op :type :ok , :value (independent/tuple k value)))
+         :write (do (v/reset! conn k v)
+                    (assoc op :type :ok))
+         :cas (let [[old new] v]
+                (assoc op :type (if (v/cas! conn k old new)
+                                  :ok
+                                  :fail))))
+       #_{:clj-kondo/ignore [:unresolved-symbol]} ;; NOTE: lsp bug
+       (catch [:errorCode 100] ex
+         (assoc op :type :fail, :error :not-found))
+       #_{:clj-kondo/ignore [:unresolved-symbol]} ;; NOTE: lsp bug
+       (catch java.net.SocketTimeoutException e
+         (assoc op
+                :type  (if (= :read (:f op))
+                         :fail
+                         :info)
+                :error :timeout)))))
+
+  (teardown! [_ _])
+
+  (close! [_ _]))
+
+(defn register-workload
+  "Tests linearizable reads, writes, and compare-and-set operations on
+  independent keys."
+  [opts]
+  {:client    (Client. nil)
+   :checker   (independent/checker
+               (checker/compose
+                {:linear   (checker/linearizable {:model     (model/cas-register)
+                                                  :algorithm :linear})
+                 :timeline (timeline/html)}))
+   :generator (independent/concurrent-generator
+               10
+               (range)
+               (fn [_]
+                 (->> (gen/mix [r w cas])
+                      (gen/limit (:ops-per-key opts)))))})
+
+(def workloads
+  "A map of workload names to functions that construct workloads, given opts."
+  {"set"      set/workload
+   "register" register-workload})
+
+(defn etcd-test
+  "Given an options map from the command line runner (e.g. :nodes, :ssh,
+  :concurrency, ...), constructs a test map."
+  [opts]
+  (let [quorum (boolean (:quorum opts))
+        workload ((get workloads (:workload opts)) opts)]
+    (merge tests/noop-test
+           opts
+           {:name (str "etcd q=" quorum " " (name (:workload opts)))
+            :os debian/os
+            :db (db "v3.1.5")
+            :pure-generators true
+            :quorum quorum
+            :client (:client workload)
+            :checker (checker/compose
+                      {:perf   (checker/perf)
+                       :indep (independent/checker
+                               (checker/compose
+                                {:workload (:checker workload)
+                                 :timeline (timeline/html)}))})
+            :nemesis (nemesis/partition-random-halves)
+            :generator (gen/phases
+                        (->> (:generator workload)
+                             (gen/stagger (/ (:rate opts)))
+                             (gen/nemesis
+                              (cycle [(gen/sleep 5)
+                                      {:type :info, :f :start}
+                                      (gen/sleep 5)
+                                      {:type :info, :f :stop}]))
+                             (gen/time-limit (:time-limit opts)))
+                        (gen/log "Healing cluster")
+                        (gen/nemesis (gen/once {:type :info, :f :stop}))
+                        (gen/log "Waiting for recovery")
+                        (gen/sleep 10)
+                        (gen/clients (:final-generator workload)))})))
+
+(def cli-opts
+  "Additional command line options."
+  [["-q" "--quorum" "Use quorum reads, instead of reading from any primary."]
+   ["-r" "--rate HZ" "Approximate number of requests per second, per thread."
+    :default  10
+    :parse-fn read-string
+    :validate [#(and (number? %) (pos? %)) "Must be a positive number"]]
+   [nil "--ops-per-key NUM" "Maximum number of operations on any given key."
+    :default  100
+    :parse-fn parse-long
+    :validate [pos? "Must be a positive integer."]]
+   ["-w" "--workload NAME" "What workload should we run?"
+    :missing  (str "--workload " (cli/one-of workloads))
+    :validate [workloads (cli/one-of workloads)]]])
+
+(defn -main
+  "Handles command line arguments. Can either run a test, or a web server for
+  browsing results."
+  [& args]
+  (cli/run! (merge (cli/single-test-cmd {:test-fn etcd-test
+                                         :opt-spec cli-opts})
+                   (cli/serve-cmd))
+            args))
---
+++ iu9-distributed-db/test/jepsen/etcdemo_test.clj
@@ -0,0 +1,6 @@
+(ns jepsen.etcdemo-test
+  (:require [clojure.test :refer [deftest is testing]]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
