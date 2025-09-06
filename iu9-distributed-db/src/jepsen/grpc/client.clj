ada5190637019f7dc48f1695fbdaa95dc7ff0411
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-09-06 22:51:21 +0300

    Impl exception handling

--- iu9-distributed-db/src/jepsen/grpc/client.clj
+++ iu9-distributed-db/src/jepsen/grpc/client.clj
@@ -1,14 +1,25 @@
 (ns jepsen.grpc.client
-  (:import [iu9db DbGrpc]))
+  (:import [iu9db DbGrpc]
+           [io.grpc StatusRuntimeException]))
 
 (def client (iu9db.DbGrpc/newBlockingStub
                (-> (io.grpc.ManagedChannelBuilder/forAddress "localhost" (int 50051))
                    (.usePlaintext)
                    .build)))
 
-(defn get-request [key]
-  (println
-   (.getValue
-    (.get client (-> (iu9db.Api$KeyRequest/newBuilder)
-                                   (.setKey key)
-                                   .build)))))
+(defn do-get [key]
+   (try
+     (.getValue
+      (.get client (-> (iu9db.Api$KeyRequest/newBuilder)
+                       (.setKey key)
+                       .build)))
+     (catch StatusRuntimeException e
+       (.getStatus e))))
+
+(defn do-insert [key value]
+  (try (.insert client (-> (iu9db.Api$KeyValueRequest/newBuilder)
+                           (.setKey key)
+                           (.setValue value)
+                           .build))
+       (catch StatusRuntimeException e
+         (.getStatus e))))
